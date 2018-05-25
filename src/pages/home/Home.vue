0dbe2653f1839718549888a459eb81945d69233b
M-cq <chen.qian.g@qq.com>
2018-05-25 17:28:11 +0800

    abc

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<scrip>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style>.title{font-size: 50px;}</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<scrip>
+export default{
+  name: 'HomeHeader' 
+}
+</script>
+<style></style>
